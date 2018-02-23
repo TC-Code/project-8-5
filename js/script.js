0bda46e35232c4b89659a3709c49dfc6ce82a38d
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2018-02-23 16:13:03 +0100

    Add Triangle area calculator with basic poject structure

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// triangle area
+var a = prompt('Enter the value of the triangle base "a"');
+var h = prompt('Enter the value of triangle height "h"');
+var triangleArea = a * h / 2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
